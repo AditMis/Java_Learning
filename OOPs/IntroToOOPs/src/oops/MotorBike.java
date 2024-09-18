c80719fde8e2e4dbe1eb6d80dc2836a6d6e326a7
CORP\aditymis <mishraaditya436@gmail.com>
2024-09-18 18:11:51 +0530

    no message

---
+++ OOPs/IntroToOOPs/src/oops/MotorBike.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class MotorBike {
+	void start() {
+		System.out.println("This function starts the bike.");
+	}
+	void stop() {
+		System.out.println("This function stops the bike.");
+	}
+	void wheelie() {
+		System.out.println("This function asks the rider to perfrom a wheelie.");
+	}
+	void mileage() {
+		System.out.println("This function calculates the mileage of the bike.");
+	}
+	void price() {
+		System.out.println("This function provides the price of the bike.");
+	}
+}
---
+++ OOPs/IntroToOOPs/src/oops/MotorBikeRunner.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class MotorBikeRunner {
+	public static void main(String[] args) {
+		MotorBike ducati= new MotorBike();
+		MotorBike honda= new MotorBike();
+		ducati.mileage();
+		ducati.start();
+		ducati.price();
+	}
+}
