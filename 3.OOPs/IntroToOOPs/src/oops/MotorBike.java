cc3a1a83195ded9b19d3ac8b9dcea9cb1266854c
CORP\aditymis <mishraaditya436@gmail.com>
2024-09-18 18:40:15 +0530

    no message

--- 3.OOPs/IntroToOOPs/src/oops/MotorBike.java
+++ 3.OOPs/IntroToOOPs/src/oops/MotorBike.java
@@ -1,6 +1,8 @@
 package oops;
 
 public class MotorBike {
+	private int speed;
+	private int mileage;
 	void start() {
 		System.out.println("This function starts the bike.");
 	}
@@ -10,10 +12,22 @@ public class MotorBike {
 	void wheelie() {
 		System.out.println("This function asks the rider to perfrom a wheelie.");
 	}
-	void mileage() {
-		System.out.println("This function calculates the mileage of the bike.");
+	void setMileage(int mileage) {
+		this.mileage=mileage;
+		System.out.println("This function calculates the mileage of the bike is " );
+	}
+	
+	int getMileage() {
+		return this.mileage;
 	}
 	void price() {
 		System.out.println("This function provides the price of the bike.");
+	}
+	void setSpeed(int speed) {
+	  this.speed=speed;
+	}
+	
+	int getSpeed() {
+		return this.speed;
 	}
 }
--- 3.OOPs/IntroToOOPs/src/oops/MotorBikeRunner.java
+++ 3.OOPs/IntroToOOPs/src/oops/MotorBikeRunner.java
@@ -4,11 +4,16 @@ public class MotorBikeRunner {
 	public static void main(String[] args) {
 		MotorBike ducati= new MotorBike();
 		MotorBike honda= new MotorBike();
-		ducati.mileage();
 		ducati.start();
 		ducati.price();
 		honda.wheelie();
 		honda.stop();
+		// Here we are using the setter to set the value of a class member i.e. a private member of a class using functions/methods.
+		ducati.setSpeed(50);
+		honda.setMileage(80);
+		// Here we are using the getter to get the value of a class member i.e a private member of a class using function/ methods.
+		System.out.println("The speed of the ducati bike is "+ ducati.getSpeed());
+		System.out.println("The mileage of the honda bike is "+ honda.getMileage());
 		
 	}
 }
